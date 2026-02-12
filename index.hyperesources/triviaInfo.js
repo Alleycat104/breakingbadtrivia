a914a806b5485b049be4229af9f15304ce22c2d7
Alleycat104 <liliorcutt@gmail.com>
2026-02-12 15:05:30 -0600

    Add files via upload

---
+++ index.hyperesources/triviaInfo.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"BREAKING BAD TRIVIA";
+	window.questions = [		"JESSES AGE", 
+								"WALTERS MIDDLE NAME",
+								"NAME OF HANKS PARTNER", 
+								"NAME OF GUS'S RESTAURANT", 
+								"NAME OF THE SONG IN THE FINAL SCENE OF THE FINAL EPISODE",
+								"NAME OF THE GUY IN THE WHEELCHAIR",
+								"SAUL GOODMANS REAL NAME",
+								"WHATS THE NAME OF THE CARWASH",
+								"WHAT DID WALT USE TO POISON BROCK",
+								"WALTER WHITES ADDRESS" ];
+	window.correctAnswer = [ 	"E", "A", "E", "D", "C", "B", "A", "D", "D", "C" ]; 
+	window.answerA = [ 			"20", 
+								"HARTWELL", 
+								"LUCUS GOMEZ", 
+								"CLUCKIN' CHICKEN", 
+								"RUBY RED",
+								"MARCO SALAMANCA",
+								"JAMES MCGILL",
+								"2B CAR WASH",
+								"RICIN",
+								"309 VINEWOOD LANE"];
+	window.answerB = [ 			"21", 
+								"HOWARD", 
+								"CALVIN WILSON", 
+								"BROTHER'S CHICKEN",
+								"FOREVER BLUE",
+								"HECTOR SALAMANCA",
+								"HENRY MCGILL",
+								"3D CAR WASH",
+								"ARSENIC",
+								"299 ALBUQUERQUE RD" ];
+	window.answerC = [ 			"22",
+								"HARRY",
+								"LOS POLLOS", 
+								"HERBERT HOOVER", 
+								"BABY BLUE",
+								"NACHO SALAMANCA",
+								"BILL MCGILL",
+								"99 CAR WASH",
+								"STRYCHNINE",
+								"308 NEGRA ARROYO LANE" ];
+	window.answerD = [ 			"23",  
+								"BENJAMIN",
+								"WILLIAM ARTHUR", 
+								"LOS POLLOS HERMANOS",
+								"NEXT TO YOU",
+								"TUCO SALAMANCA",
+								"SAUL GOODMAN",
+								"A1A CAR WASH",
+								"LILY OF THE VALLEY",
+								"304 HAMILTION LANE" ];
+	window.answerE = [ 			"24", 
+								"ALEXANDER", 
+								"STEVE GOMEZ", 
+								"GUS'S CHICKEN", 
+								"CLOSE TO YOU",
+								"STEWART SALAMANCA",
+								"BRANDON MAYHEW",
+								"2D CAR WASH",
+								"MERCURY",
+								"202 CREEK RD" ];
+	window.response = [ 		"JESSE IS 24 YEARS OLD IN THE FIRST SEASON OF BREAKING BAD", 
+								"HARTWELL IS WALTERS MIDDLE NAME", 
+								"STEVE 'GOMEY' GOMEZ IS HANKS PARTNERS NAME", 
+								"GUS'S RESTAURANT IS LOS POLLOS HERMOS", 
+								"BABY BLUE IS THE NAME OF THE FINAL SONG PLAYED",
+								"HECTOR SALAMANCA IS THE OLD MAN IN THE WHEELCHAIR",
+								"JAMES 'JIMMY' MCGILL IS SAULS REAL NAME",
+								"A1A IS THE NAME OF THE CAR WASH",
+								"WALT USED LILY OF THE VALLEY TO POSION BROCK",
+								"WALTERS ADDRESS IS 308 NEGRA ARROYO LANE, ALBUQUERQUE, NEW MEXICO" ];
+							
+}
+						
